1920e42d183d90498a761d4bef648670b2aa0570
Terry Chen <gn02090867@hotmail.com>
2016-07-20 23:45:53 -0400

    changing port

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ var twitter = function() {
     var self = this;
 
     self.setupVariables = function() {
-        self.port = process.env.PORT || 1050;
+        self.port = process.env.PORT || 80;
     };
 
     self.initialize = function() {
